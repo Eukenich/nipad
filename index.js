16cb56b9571d33efbab3ed1b000a78f4ee293de0
Eukenich <artem.a@cronix.ms>
2017-05-02 11:38:20 +0300

    Add count for symbols and words

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 ﻿angular.module('nikitaPad', ['ui.bootstrap.modal'])
-    .controller('npCtrl', function ($scope) {
-        $scope.localKeys = localStorage.localkeys
-        $scope.texts = (angular.isUndefined($scope.localKeys)) ? [] : $scope.localKeys.split(',')
-        $scope.dist = ''
-        $scope.downloadText = function (textDist) {
-            $scope.dist = textDist
-            $scope.loaded = (JSON.parse(localStorage.getItem($scope.dist))) ? JSON.parse(localStorage.getItem($scope.dist)) : alert('Nothing to download!')
-        }
+  .controller('npCtrl', function ($scope) {
+    $scope.localKeys = localStorage.localkeys
+    $scope.texts = (angular.isUndefined($scope.localKeys)) ? [] : $scope.localKeys.split(',')
+    $scope.textInfo = {sym:'0', words:'0'}
+    //$scope.hideInfo = false
+    $scope.downloadText = function (textDist) {
+      let dist = textDist
+      $scope.loaded = (JSON.parse(localStorage.getItem(dist))) ? JSON.parse(localStorage.getItem(dist)) : alert('Nothing to download!')
+    }
 
-        $scope.addText = function (textLocalKey) {
-            $scope.texts.push(textLocalKey)
-            localStorage.localkeys = $scope.texts
-        }
+    $scope.addText = function (textLocalKey) {
+      $scope.texts.push(textLocalKey)
+      localStorage.localkeys = $scope.texts
+    }
 
-        $scope.dellText = function (textLocalKey) {
-            $scope.dist = textLocalKey
-            $scope.texts.splice($scope.texts.indexOf(textLocalKey),1)
-            localStorage.localkeys = $scope.texts
-            localStorage.removeItem($scope.dist)
-        }
+    $scope.dellText = function (textLocalKey) {
+      let dist = textLocalKey
+      $scope.texts.splice($scope.texts.indexOf(textLocalKey), 1)
+      localStorage.localkeys = $scope.texts
+      localStorage.removeItem(dist)
+    }
 
-        $scope.uploadText = function (textLocalKey, text) {
-            $scope.dist = textLocalKey
-            console.log($scope.dist)
-            if (text) {
-                localStorage.setItem($scope.dist, JSON.stringify(text))
-            }
-            else {
-                alert('Text area is empty!')
-            }
-        }
+    $scope.uploadText = function (textLocalKey, text) {
+      let dist = textLocalKey
+      if (text) {
+        localStorage.setItem(dist, JSON.stringify(text))
+      }
+      else {
+        alert('Text area is empty!')
+      }
+    }
 
-    });
+    $scope.loadedLength = function (textDist) {
+      let dist = textDist
+      let load = (JSON.parse(localStorage.getItem(dist))) ? JSON.parse(localStorage.getItem(dist)) : ''
+      let loaded = load.text
+     // $scope.hideInfo = !$scope.hideInfo
+      $scope.textInfo.sym = (angular.isUndefined(loaded)) ? 0 : loaded.length
+      $scope.textInfo.words = (angular.isUndefined(loaded)) ? 0 : loaded.split(' ').length
+
+    }
+  });
